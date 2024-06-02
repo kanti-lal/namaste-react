60bd899d4e449ce4fded32a2ff9f6cc5558b9edd
kanti-lal <kanti.ylocation@gmail.com>
2024-06-02 21:01:41 +0530

    chapt 10

--- Chapter 10 - jo dikhta hai vo bikta hai/Coding/Components/Grocery.js
+++
@@ -1,9 +0,0 @@
-const Grocery = () => {
-  return (
-    <div className='groceryMain'>
-      This is a big grocery store and that have multiple child component!
-    </div>
-  );
-};
-
-export default Grocery;
